e333729201f4d110e23d6e6b2c47282885ba38fd
Sauce101 <michaelsaucedo443@gmail.com>
2022-04-05 00:51:30 -0700

    obverse and reverse of card face the same direction while flipping

--- src/components/cardData/ComputerCardData.js
+++ src/components/cardData/ComputerCardData.js
@@ -10,29 +10,56 @@ const ComputerCardData = ({ computerCard, ...cardsize }) => {
     from: { transform: "rotateY(0deg)" },
     to: { transform: "rotateY(360deg)" },
   })
+  const backsideLandscape = keyframes({
+    from: { transform: "rotateY(180deg)" },
+    to: { transform: "rotateY(540deg)" },
+  })
 
   return (
-    <Card
-      sx={{
-        "@media (orientation: portrait)": {
-          maxWidth: cardsize.cardWidthP,
-          borderRadius: cardsize.radius,
-          mx: "auto",
-          transform: "rotate(-90deg)",
-          animation: `${rotationPortrait} .6s 1 ease-out`,
-        },
-        "@media (orientation: landscape)": {
-          maxWidth: cardsize.cardWidthL,
-          borderRadius: cardsize.radius,
-          mr: "auto",
-          ml: 4,
-          animation: `${rotationLandscape} .6s 1 ease-out`,
-          transformStyle: "preserve-3d",
-        },
-      }}
-    >
-      <CardMedia component="img" src={computerCard} alt="card" />
-    </Card>
+    <>
+      <Card
+        sx={{
+          "@media (orientation: portrait)": {
+            maxWidth: cardsize.cardWidthP,
+            borderRadius: cardsize.radius,
+            mx: "auto",
+            transform: "rotate(-90deg)",
+            animation: `${rotationPortrait} .6s 1 ease-out`,
+          },
+          "@media (orientation: landscape)": {
+            maxWidth: cardsize.cardWidthL,
+            borderRadius: cardsize.radius,
+            marginLeft: "15%",
+            animation: `${rotationLandscape} .7s 1 linear`,
+            transformStyle: "preserve-3d",
+            backfaceVisibility: "hidden",
+            position: "absolute",
+            width: "100%",
+          },
+        }}
+      >
+        <CardMedia component="img" src={computerCard} alt="card" />
+      </Card>
+      <Card
+        sx={{
+          "@media (orientation: portrait)": {
+            display: "none",
+          },
+          "@media (orientation: landscape)": {
+            maxWidth: cardsize.cardWidthL,
+            borderRadius: cardsize.radius,
+            marginLeft: "15%",
+            animation: `${backsideLandscape} .7s 1 linear`,
+            transformStyle: "preserve-3d",
+            backfaceVisibility: "hidden",
+            position: "absolute",
+            width: "100%",
+          },
+        }}
+      >
+        <CardMedia component="img" src={computerCard} alt="card" />
+      </Card>
+    </>
   )
 }
 
--- src/components/cardData/PlayerCardData.jsx
+++ src/components/cardData/PlayerCardData.jsx
@@ -11,29 +11,56 @@ const PlayerCardData = ({ playerCard, ...cardsize }) => {
     from: { transform: "rotateY(0deg)" },
     to: { transform: "rotateY(360deg)" },
   })
+  const backsideLandscape = keyframes({
+    from: { transform: "rotateY(180deg)" },
+    to: { transform: "rotateY(540deg)" },
+  })
 
   return (
-    <Card
-      sx={{
-        "@media (orientation: portrait)": {
-          maxWidth: cardsize.cardWidthP,
-          borderRadius: cardsize.radius,
-          mx: "auto",
-          transform: "rotate(-90deg)",
-          animation: `${rotationPortrait} .6s 1 ease-out`,
-        },
-        "@media (orientation: landscape)": {
-          maxWidth: cardsize.cardWidthL,
-          borderRadius: cardsize.radius,
-          ml: "auto",
-          mr: 4,
-          animation: `${rotationLandscape} .6s 1 ease-out`,
-          transformStyle: "preserve-3d",
-        },
-      }}
-    >
-      <CardMedia component="img" src={playerCard} alt="card" />
-    </Card>
+    <>
+      <Card
+        sx={{
+          "@media (orientation: portrait)": {
+            maxWidth: cardsize.cardWidthP,
+            borderRadius: cardsize.radius,
+            mx: "auto",
+            transform: "rotate(-90deg)",
+            animation: `${rotationPortrait} .6s 1 ease-out`,
+          },
+          "@media (orientation: landscape)": {
+            maxWidth: cardsize.cardWidthL,
+            borderRadius: cardsize.radius,
+            marginLeft: "30%",
+            animation: `${rotationLandscape} .6s 1 linear`,
+            transformStyle: "preserve-3d",
+            backfaceVisibility: "hidden",
+            position: "absolute",
+            width: "100%",
+          },
+        }}
+      >
+        <CardMedia component="img" src={playerCard} alt="card" />
+      </Card>
+      <Card
+        sx={{
+          "@media (orientation: portrait)": {
+            display: "none",
+          },
+          "@media (orientation: landscape)": {
+            maxWidth: cardsize.cardWidthL,
+            borderRadius: cardsize.radius,
+            marginLeft: "30%",
+            animation: `${backsideLandscape} .6s 1 linear`,
+            transformStyle: "preserve-3d",
+            backfaceVisibility: "hidden",
+            position: "absolute",
+            width: "100%",
+          },
+        }}
+      >
+        <CardMedia component="img" src={playerCard} alt="card" />
+      </Card>
+    </>
   )
 }
 
