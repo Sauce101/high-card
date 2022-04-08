b0186c7067e89c4b1359d6fd859d93891834c123
Sauce101 <michaelsaucedo443@gmail.com>
2022-04-07 22:25:27 -0700

    Sets import from renamed to deck

--- src/components/SetThree.jsx
+++ src/components/SetThree.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 import { Grid } from "@mui/material"
-import Deck from "./decks/OldDeck"
+import Deck from "./deck/OldDeck"
 import PlayerCardData from "./cardData/PlayerCardData"
 import ComputerCardData from "./cardData/ComputerCardData"
 import DeckCardData from "./cardData/DeckCardData"
